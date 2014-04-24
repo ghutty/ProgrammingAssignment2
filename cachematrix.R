6c8884d277f637c0e75ce81821946e024732aa75
ghutty <ghutty@wilhelm-guzmans-macbook.local>
2014-04-24 01:11:43 -0700

    R Programming Peer Assignment
    R Programming Peer Assignment - Create 2 functions.  makeCacheMatrix()
    function to create a matrix which has 4 functions to set and get the
    created matrix and to set and get the inverse of the created matrix.
    cacheSolve() function is to return the cached inverse matrix if
    available or  run a solve() function on the retrieved matrix and set
    and return the inverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix function takes a matrix and sets it as a Cached Matrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## initialize 'm' variable as the Inverse Matrix Cache with NULL value
+  m <- NULL  
+  
+  ## setmatrix() function definition
+  setmatrix <- function(y) {
+    ## call to setmatrix replaces 'x' variable Matrix Cached Value with the passed matrix
+    ## searches 'x' in the parent environment and redefines the value
+    x <<- y
+    ## call to setmatrix replaces 'm' variable Inverse Matrix Cached value to NULL
+    ## searches 'm' in the parent environment and redefines the value to NULL
+    m <<- NULL
+  }
+  
+  ## returns 'x' Cached Matrix Value
+  getmatrix <- function() x
+  
+  ## sets 'm' Cached Inverse Matrix Value with the passed solved inverse matrix value from cacheSolve
+  setinvmatrix <- function(solvedinvmtx) m <<- solvedinvmtx
+  
+  ## returns 'm' Cached Inverse Matrix Value
+  getinvmatrix <- function() m
+  
+  ##if no arguments are passed, show list of Cache Matrix functions
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinvmatrix = setinvmatrix,
+       getinvmatrix = getinvmatrix)
 }
 
-
-## Write a short comment describing this function
-
+## cachSolve function takes a Cached Matrix Object and returns the Inverse of the Cached Matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvmatrix()
+  
+  ## if 'm' is not NULL, exit and return the cached inverse matrix retrieved from 'x'
+  if(!is.null(m)) {
+    message("getting cached inverse matrix data")
+    return(m)
+  }
+  
+  ## if 'm' is NULL, retrieve the cached matrix in 'x' to 'data'
+  data <- x$getmatrix()
+  ## use solve() to get the inverse matrix of 'data' and assign to 'm'
+  m <- solve(data, ...)
+  ## set the inverse matrix of 'x' with the 'm' solved value using setinvmatrix()
+  x$setinvmatrix(m)
+  ## return 'a' value - inverse matrix
+  m
 }
